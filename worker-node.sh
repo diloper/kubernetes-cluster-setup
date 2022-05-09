e7166a59741bebd16a0a23e050f098acf10c40f3
diloper <diloper@users.noreply.github.com>
2022-05-09 21:16:40 +0800

    Update worker-node.sh
    update  NVIDIA driver  install method

--- worker-node.sh
+++ worker-node.sh
@@ -6,24 +6,8 @@
 sudo apt-get purge nvidia*
 
 # Install NVIDIA drivers (using package manager - recommended)
-sudo apt-get install linux-headers-$(uname -r)
-distribution=$(. /etc/os-release;echo $ID$VERSION_ID | sed -e 's/\.//g')
-wget https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/cuda-$distribution.pin
-sudo mv cuda-$distribution.pin /etc/apt/preferences.d/cuda-repository-pin-600
-sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/7fa2af80.pub
-echo "deb http://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64 /" | sudo tee /etc/apt/sources.list.d/cuda.list
-sudo apt-get update
-sudo apt-get -y install cuda-drivers
-
-# Install NVIDIA drivers (using run file - not necessary if installed already using package manager)
-BASE_URL=https://us.download.nvidia.com/tesla
-DRIVER_VERSION=450.80.02
-curl -fSsl -O $BASE_URL/$DRIVER_VERSION/NVIDIA-Linux-x86_64-$DRIVER_VERSION.run
-sudo chmod 777 NVIDIA-Linux-x86_64-$DRIVER_VERSION.run
-sudo service lightdm stop
-sudo init 3
-sudo ./NVIDIA-Linux-x86_64-$DRIVER_VERSION.run
-sudo apt install linux-headers-$(uname -r)
+curl https://raw.githubusercontent.com/GoogleCloudPlatform/compute-gpu-installation/main/linux/install_gpu_driver.py --output install_gpu_driver.py
+sudo python3 install_gpu_driver.py
 
 # Purge all of kubernetes and docker: (not required if working on fresh instance)
 sudo apt-get remove docker docker-engine docker.io containerd runc
